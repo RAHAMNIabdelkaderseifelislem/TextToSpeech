0345e11ead5b2b80fbff78b9d7ce23543e9cb442
RAHMANI Abd El Kader Seif El Islem <77025510+RAHAMNIabdelkaderseifelislem@users.noreply.github.com>
2021-04-16 17:03:44 +0100

    Add files via upload

---
+++ textToSpeech.py
@@ -0,0 +1,12 @@
+from gtts import gTTS 
+import os
+
+def textToSpeech(text):
+    language = 'en'
+    speech = gTTS(text = text, lang = language, slow = False)
+    speech.save("text.mp3")
+    os.system("start text.mp3")
+
+text = input("enter your text here:")
+
+textToSpeech(text)
